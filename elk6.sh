7437298ee9e74758be579dac8336236fe54cfded
Frederic MARTIN <frederic@martin.lc>
2018-06-25 16:59:52 +0200

    Update and rename elk6.preps to elk6.sh

--- elk6.sh
+++ elk6.sh
@@ -12,6 +12,23 @@ curl -X PUT "localhost:9200/logstash-sexilog" -H 'Content-Type: application/json
 }
 '
 
+set -euo pipefail
+url="http://localhost:5601"
+index_pattern="logstash-*"
+id="logstash-*"
+time_field="@timestamp"
+
+# Create index pattern
+# curl -f to fail on error
+curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+  "$url/api/saved_objects/index-pattern/$id" \
+  -d"{\"attributes\":{\"title\":\"$index_pattern\",\"timeFieldName\":\"$time_field\"}}"
+
+# Make it the default index
+curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+  "$url/api/kibana/settings/defaultIndex" \
+  -d"{\"value\":\"$id\"}"
+
 # Retrieve json data for index pattern
 curl -X GET "localhost:5601/api/index_patterns/_fields_for_wildcard?pattern=logstash-*" 
 
