5d5bde30225def0867a300ca942e6d3bc8d8cf33
Frederic MARTIN <frederic@martin.lc>
2018-06-25 17:55:15 +0200

    Update elk6.sh

--- elk6.sh
+++ elk6.sh
@@ -4,15 +4,54 @@ curl -X PUT "localhost:9200/logstash-sexilog" -H 'Content-Type: application/json
   "mappings": {
     "doc": { 
       "properties": { 
-        "dummyfieldtext3": {
-          "type": "text",
-          "fields": {
-            "raw": { 
-              "type":  "keyword"
-            }
-          }
-        },
-        "dummyfieldint3":  { "type": "integer" }
+        "Correlator": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "alert": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "body_type_3": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "body_type_6": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "body_type_7": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "canonical_name": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "com_vmware": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "dashboard": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "datastore_name": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "esx_audit": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "esx_clear": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "esx_problem": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "failed_to": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "group": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "host": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "hostname": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "location": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "mbps": { "type": "integer" },
+        "message": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "message_body": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "message_debug": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "message_program": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "message_syslog": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "microseconds": { "type": "integer" },
+        "milliseconds": { "type": "integer" },
+        "msg": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "nmp": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "pid": { "type": "integer" },
+        "program": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "runtime_name": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "scsi_code": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "sense_data": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "syslog_facility": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "syslog_facility_code": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "syslog_pri": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "syslog_severity": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "syslog_severity_code": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "tags": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "title": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "type": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "user": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vim": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vm_name": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vmfs_uuid": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vmnic": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vmodl": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vmx_name": { "type": "text", "fields": { "raw": { "type":  "keyword" } } },
+        "vob": { "type": "text", "fields": { "raw": { "type":  "keyword" } } }
       }
     }
   }
