2353a321651a34defa48eae392e44a46f1df484a
Frederic MARTIN <frederic@martin.lc>
2018-06-25 17:31:03 +0200

    Update elk6.sh

--- elk6.sh
+++ elk6.sh
@@ -4,7 +4,14 @@ curl -X PUT "localhost:9200/logstash-sexilog" -H 'Content-Type: application/json
   "mappings": {
     "doc": { 
       "properties": { 
-        "dummyfieldtext3": { "type": "text"  }, 
+        "dummyfieldtext3": {
+          "type": "text",
+          "fields": {
+            "raw": { 
+              "type":  "keyword"
+            }
+          }
+        },
         "dummyfieldint3":  { "type": "integer" }
       }
     }
@@ -30,8 +37,8 @@ curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
   -d"{\"value\":\"$id\"}"
 
 # Retrieve json data for index pattern
-curl -X GET "localhost:5601/api/index_patterns/_fields_for_wildcard?pattern=logstash-*" 
+# curl -X GET "localhost:5601/api/index_patterns/_fields_for_wildcard?pattern=logstash-*" 
 
 # Refresh index pattern by putting these data in the index pattern
 # the index pattern 5cc05bc0-7594-11e8-aac5-f7fc6de727b6 should be retrieve programaticaly
-curl -X PUT 'localhost:5601/api/saved_objects/index-pattern/5cc05bc0-7594-11e8-aac5-f7fc6de727b6' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'kbn-xsrf: true' --data-binary '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp","fields":"[#####blablafield#####]"}}' --compressed
+# curl -X PUT 'localhost:5601/api/saved_objects/index-pattern/5cc05bc0-7594-11e8-aac5-f7fc6de727b6' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'kbn-xsrf: true' --data-binary '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp","fields":"[#####blablafield#####]"}}' --compressed
