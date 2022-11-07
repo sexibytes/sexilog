c58fcfa6767ee148ea9af806752247265bd1a23b
Raphaël SCHITZ <raphael@schitz.net>
2022-11-07 14:36:49 +0100

    Update PullGuestInfo.sh

--- opt/sexilog/PullGuestInfo.sh
+++ opt/sexilog/PullGuestInfo.sh
@@ -1,68 +1,59 @@
 #!/bin/bash
-
-GUESTINFO=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|xml_grep 'Kind' --text_only)
+#
+GUESTINFO=$(xml_grep 'Kind' <(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv") --text_only)
 
 if [[ $GUESTINFO =~ "VMware ESXi" ]]; then
 
-  GUESTIP=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.ipaddress|awk -F'"' '{ print $4 }')
-  GUESTMASK=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.netmask|awk -F'"' '{ print $4 }')
-  GUESTGW=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.gateway|awk -F'"' '{ print $4 }')
+        GUESTIP=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.ipaddress|awk -F'"' '{ print $4 }')
+        GUESTMASK=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.netmask|awk -F'"' '{ print $4 }')
+        GUESTGW=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.gateway|awk -F'"' '{ print $4 }')		
 
-  if [[ $GUESTIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ $GUESTMASK =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ $GUESTGW =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        if [[ $GUESTIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ $GUESTMASK =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [[ $GUESTGW =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 
-    GUESTNS=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.dns|awk -F'"' '{ print $4 }')
-    GUESTDNS=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.domain|awk -F'"' '{ print $4 }')
-    GUESTNAME=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.hostname|awk -F'"' '{ print $4 }')
+                GUESTNS=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.dns|awk -F'"' '{ print $4 }')
+                GUESTDNS=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.domain|awk -F'"' '{ print $4 }')
+                GUESTNAME=$(/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfEnv"|grep guestinfo.hostname|awk -F'"' '{ print $4 }')
 
-    echo "auto lo" > /tmp/tmp-interfaces
-    echo "iface lo inet loopback" >> /tmp/tmp-interfaces
-    echo "allow-hotplug eth0" >> /tmp/tmp-interfaces
-    echo "iface eth0 inet static" >> /tmp/tmp-interfaces
-    echo " address $GUESTIP" >> /tmp/tmp-interfaces
-    echo " netmask $GUESTMASK" >> /tmp/tmp-interfaces
-    echo " gateway $GUESTGW" >> /tmp/tmp-interfaces
+                echo "auto lo" > /etc/network/interfaces
+                echo "iface lo inet loopback" >> /etc/network/interfaces
+                echo "allow-hotplug eth0" >> /etc/network/interfaces
+                echo "iface eth0 inet static" >> /etc/network/interfaces
+                echo " address $GUESTIP" >> /etc/network/interfaces
+                echo " netmask $GUESTMASK" >> /etc/network/interfaces
+                echo " gateway $GUESTGW" >> /etc/network/interfaces
+				
+				if [[ $GUESTNS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
+				
+					echo " dns-nameservers $GUESTNS" >> /etc/network/interfaces
+				
+				fi
+				
+				if [[ -n $GUESTDNS ]]; then
+					
+					echo " dns-search $GUESTDNS" >> /etc/network/interfaces
+				fi
+				
 
-    if [[ $GUESTNS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
-    	echo " dns-nameservers $GUESTNS" >> /tmp/tmp-interfaces
-    fi
+                echo "127.0.0.1   localhost" > /etc/hosts
+                echo "$GUESTIP   $GUESTNAME" >> /etc/hosts
+                echo "$GUESTNAME" > /etc/hostname
 
-    if [[ -n $GUESTDNS ]]; then
-    	echo " dns-search $GUESTDNS" >> /tmp/tmp-interfaces
-    fi
+                hostname $GUESTNAME
 
-    if cmp -s "/tmp/tmp-interfaces" "/etc/network/interfaces"; then
-      rm -f /tmp/tmp-interfaces
-    else
-      cp -f /tmp/tmp-interfaces /etc/network/interfaces
-      needUpdate=true
-    fi
+                /etc/init.d/networking stop
+                /etc/init.d/networking start
+                /etc/init.d/resolvconf stop
+                /etc/init.d/resolvconf start
 
-    echo "127.0.0.1   localhost" > /tmp/tmp-hosts
-    echo "$GUESTIP   $GUESTNAME" >> /tmp/tmp-hosts
+                ifdown eth0
+                ifup eth0
 
-    if cmp -s "/tmp/tmp-hosts" "/etc/hosts"; then
-      rm -f /tmp/tmp-hosts
-    else
-      cp -f /tmp/tmp-hosts /etc/hosts
-      needUpdate=true
-    fi
+        fi
 
-    echo "$GUESTNAME" > /tmp/tmp-hostname
+fi
 
-    if cmp -s "/tmp/tmp-hostname" "/etc/hostname"; then
-      rm -f /tmp/tmp-hostname
-    else
-      cp -f /tmp/tmp-hostname /etc/hostname
-      needUpdate=true
-    fi
-
-    if [[ $needUpdate ]]; then
-      hostname $GUESTNAME
-      /etc/init.d/networking stop && /etc/init.d/networking start
-      /etc/init.d/resolvconf stop && /etc/init.d/resolvconf start
-
-      ifdown eth0 && ifup eth0
-    fi
-  fi
-
+# https://kb.vmware.com/s/article/1014038
+vmwtimesync=$(vmware-toolbox-cmd timesync status)
+if [[ $vmwtimesync =~ "Disabled" ]]; then
+        vmware-toolbox-cmd timesync enable
 fi
